71df14bf98a94504f81628f53fd6902cf97e3b00
Cris575 <cg691539@gmail.com>
2025-05-29 19:12:42 -0600

    Implement structural updates and optimizations across multiple modules

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const $ = element => document.querySelector(element);
+const $$ = elements => document.querySelectorAll(elements);
+
+// $('.number').textContent = '13';
+// $('.score').textContent = '10';
+// console.log($('.guess').value);
+// $('.guess').value = 23;
+// console.log($('.guess').value);
+
+let count = 0;
+
+$('.check').addEventListener('click', function () {
+  $('.guess').value = count += 1;
+});
