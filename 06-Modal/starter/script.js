cc8720408d72eb3c2872532329e209122af85def
Cris575 <cg691539@gmail.com>
2026-02-12 19:53:32 -0600

    feat: implement basic modal window functionality with open, close, and escape key handling

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -8,6 +8,26 @@ const overlay = $('.overlay');
 const btnCloseModal = $('.close-modal');
 const btnsOpenModal = $$('.show-modal');
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < 3; i++) {
-    console.log(btnsOpenModal[i].textContent)
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
