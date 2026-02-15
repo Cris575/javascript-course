2174fc975fc5e9d43c02dd3307c1db75f491c488
Cris575 <cg691539@gmail.com>
2026-02-15 17:37:13 -0600

    feat: Initialize game state and implement player turn logic for Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -18,10 +18,29 @@ const player = {
 const diceElement = $('.dice');
 const currentScore = [$('#current--0'), $('#current--1')];
 const score = [$('#score--0'), $('#score--1')];
+const playerScreen = [$('.player--0'), $('.player--1')];
 
 let diceFace = 0;
+let diceValue = 0;
 // let numberOfAttempts = 0;
 let currentPlayer = 0;
+
+const init = function () {
+  diceFace = 0;
+  diceValue = 0;
+  currentPlayer = 0;
+
+  player[0].score = 0;
+  player[1].score = 0;
+
+  score[0].innerText = 0;
+  score[1].innerText = 0;
+
+  currentScore[0].innerText = 0;
+  currentScore[1].innerText = 0;
+
+  ToggleSacreen();
+};
 
 const GetRandomNumber = function () {
   return Math.floor(Math.random() * (6 - 1 + 1) + 1);
@@ -32,6 +51,7 @@ const SetDiceImage = function (value) {
 };
 
 const SetValueOfDice = function (playerNumber, value) {
+  console.log(value);
   currentScore[playerNumber].innerText = value;
 };
 
@@ -43,19 +63,48 @@ const SetEcoreOfPlayer = function (playerNumber) {
   score[playerNumber].innerText = player[playerNumber].score;
 };
 
+const ChangePlayerTurn = function () {
+  currentPlayer = currentPlayer == 1 ? 0 : 1;
+  ToggleSacreen();
+  diceValue = 0;
+};
+
+const ToggleSacreen = function () {
+  playerScreen[0].classList.toggle('player--active');
+  playerScreen[1].classList.toggle('player--active');
+};
+
+const EndGame = function (playerNumber) {
+  return player[playerNumber].score >= 100;
+};
+
 $('.btn--roll').addEventListener('click', function () {
   diceFace = GetRandomNumber();
-  console.log(diceFace);
-  if (diceFace == 1) {
-    currentPlayer = currentPlayer == 1 ? 0 : 1;
-  }
-  console.log('jugador: ' + currentPlayer);
+  diceValue += diceFace;
 
+  if (diceFace == 1) {
+    diceValue = 0;
+    SetValueOfDice(currentPlayer, diceValue);
+    ChangePlayerTurn();
+    SetDiceImage(diceFace);
+    return;
+  }
+
+  SetValueOfDice(currentPlayer, diceValue);
   SetDiceImage(diceFace);
-  SetValueOfDice(currentPlayer, diceFace);
 });
 
 $('.btn--hold').addEventListener('click', function () {
-  SaveValueOfDice(currentPlayer, diceFace);
+  if (EndGame(currentPlayer)) {
+    playerScreen[currentPlayer].classList.remove('player--active');
+    playerScreen[currentPlayer].classList.add('player--winner');
+    return;
+  }
+
+  SaveValueOfDice(currentPlayer, diceValue);
+  SetValueOfDice(currentPlayer, 0);
   SetEcoreOfPlayer(currentPlayer);
+  ChangePlayerTurn();
 });
+
+$('.btn--new').addEventListener('click', init);
