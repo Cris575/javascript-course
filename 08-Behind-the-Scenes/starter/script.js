ac95d4352a31889b4fefb5762ee9f5395ee18ea7
Cris575 <cg691539@gmail.com>
2026-02-19 22:30:30 -0600

    feat: enhance calAge function with improved output formatting and millennial check

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function calAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'New ';
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 2));
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calAge(1991);
+//console.log(age);
+//printAge();
