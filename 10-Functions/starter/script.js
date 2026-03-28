3e95401454e8a217a5956fc5e69c4d9d46b84a53
Cris <cg691539@gmail.com>
2026-03-28 13:54:12 -0600

    refactor: Comment out booking creation function and related examples in script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,26 +1,54 @@
 'use strict';
 
 // Parámetros por defecto: si no se provee un valor, se usan los definidos aquí
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassagers = 1,
-  price = 199 * numPassagers,
-) {
-  // Crear un objeto de reserva con shorthand properties
-  const booking = {
-    flightNum,
-    numPassagers,
-    price,
-  };
+// const bookings = [];
+// const createBooking = function (
+//   flightNum,
+//   numPassagers = 1,
+//   price = 199 * numPassagers,
+// ) {
+//   // Crear un objeto de reserva con shorthand properties
+//   const booking = {
+//     flightNum,
+//     numPassagers,
+//     price,
+//   };
 
-  console.log(booking);
-  bookings.push(booking);
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123'); // usa numPassagers=1 y price=199
+// createBooking('LH123', 2, 800); // todos los valores son explícitos
+// createBooking('LH123', 2); // price se calcula como 199 * 2
+// createBooking('LH123', 5); // price se calcula como 199 * 5
+// createBooking('LH123', undefined, 1000); // omite numPassagers para usar el valor por defecto
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas',
+  passport: 22034654654,
 };
 
-createBooking('LH123'); // usa numPassagers=1 y price=199
-createBooking('LH123', 2, 800); // todos los valores son explícitos
-createBooking('LH123', 2); // price se calcula como 199 * 2
-createBooking('LH123', 5); // price se calcula como 199 * 5
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'lh999';
+  passenger.name = 'Mr. ' + passenger.name;
 
-createBooking('LH123', undefined, 1000); // omite numPassagers para usar el valor por defecto
+  if (passenger.passport === 22034654654) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(jonas);
