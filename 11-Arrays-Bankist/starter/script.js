3b02d5750bac7d4d3a1df30dacb7d68d5ae24e2a
Cris <cg691539@gmail.com>
2026-04-08 16:06:43 -0600

    refactor: Add currency conversion and movement description logic in script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -425,3 +425,24 @@ movements.forEach(mov => {
 
 //! Map → como un objeto mejorado
 //! Set → para eliminar duplicados
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) {
+  movementsUSDfor.push(mov * eurToUsd);
+}
+console.log(movementsUSDfor);
+
+const movementDescriptions = movements.map(
+  (mov, index, arr) =>
+    `Movement ${index + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`,
+);
+
+console.log(movementDescriptions);
