b99dfc0450cadd37d47a2d1f9dc16dd800100174
Cris575 <cg691539@gmail.com>
2026-06-07 16:35:58 -0600

    refactor: Organize and log selected DOM elements for better clarity

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,23 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// --------------------
+//Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('#section--1');
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elments
+// .insertAdjancentHTML
